acb88b2eac817279202eee2219937f694c8afcbd
Thomas Jakway <tjakway@nyu.edu>
2017-11-01 09:41:05 -0400

    Driver now takes multiple source files

--- src/main/scala/com/jakway/ctbash/compile/Driver.scala
+++ src/main/scala/com/jakway/ctbash/compile/Driver.scala
@@ -1,15 +1,19 @@
 package com.jakway.ctbash.compile
 
+import java.io.File
 import java.nio.file.Files
 
 import com.jakway.ctbash.parser.ScalaExtractor
 
 //TODO: implement Compiler
-class Driver(val source: String) {
+class Driver(val sourceFiles: Seq[File]) {
+  val sources: Seq[String] = sourceFiles.map(scala.io.Source.fromFile(_).mkString)
 
   def run() = {
     for {
-      (bashSrc, scalaSrc) <- ScalaExtractor.extractScala(source).right
+      //TODO: write collapseEithers with a generic error type
+      (bashSrc, scalaSrc) <- sources.map(ScalaExtractor.extractScala(_))
+
     } yield {
       ???
     }
